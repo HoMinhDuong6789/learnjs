74ab1f91ec3be6602fd1cc2167e5e8c2a10d681f
Duong Ho <duong.ho@infodation.vn>
2022-07-16 15:07:35 +0700

    Lesson2 operator
    Learn operator

---
+++ Lesson01-syntax/lesson2_Operator.js
@@ -0,0 +1,42 @@
+/**
+ * CÁC LOẠI TOÁN TỬ TRONG JAVA SCRIPT
+ * 
+ * toán tử js gồm có các toán tử số học, các toán tử so sánh, toán tử logic và các toán tử với chuỗi kí tự
+ * 
+ * 1. Các toán tử gán trong js:
+ *    =
+ *    +=
+ *    -=
+ *    *=
+ *    /=
+ *    %=
+ * 
+ * 2. Toán tử số học:
+ *      +, -, *, /, %(chia lấy phần dư modulus), ++, --
+ * 
+ * 3. Toán tử so sánh:
+ *    toán tử so sánh sử dụng trong các biểu thức về logic để so sánh bằng nhau, khác nhau,...
+ *     ==
+ *     === : giống nhau(cùng giá trị và kiểu dữ liệu)
+ *     !=: khác giá trị
+ *     !===: khác giá trị và kiểu
+ *     >, <, >=, =<
+ * 
+ * 4. Toán tử logic:
+ *      &&, ||, !
+ * 
+ * 5. Toán tử điều kiện:
+ *    variable = (condition) ? value1: value2
+ * 
+ * 
+ * 6. toán tử với chuỗi:
+ *    + : nối chuỗi
+ *    ``: xuất biến ra chuỗi với kĩ thuật Template Literal:
+ *      let name = `Wellcome to ${nameValue} to Pug Bar`;
+ * 
+ * 7. Toán tử typeOf(): trả về kiểu dữ liệu cần kiểu tra của một biết, một giá trị
+ * 
+ * 
+ * 
+ *   
+ */
