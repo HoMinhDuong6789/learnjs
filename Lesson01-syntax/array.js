e9c92df44ffa62947436b85b8f793a06b650c30d
Duong Ho <duong.ho@infodation.vn>
2022-07-16 14:55:04 +0700

    Lesson1 datatype
    var, let, const, datatype

--- Lesson01-syntax/array.js
+++ Lesson01-syntax/array.js
@@ -1,6 +1,45 @@
 // var colors;
 // colors=["white", "black","blue"];
 
+/**
+ * 
+ * kiểu dữ liệu trong js:
+ * kiểu dữ liệu nguyên thuỷ
+ *  Number:
+ *  String:
+ *  Boolean:
+ *  Undefine:
+ *  Null:
+ * 
+ * Kiểu dữ liệu ko nguyên thuỷ:
+ *  Object
+ *  Array
+ *  RegExp 
+ * 
+ * Kiểu String:
+ *  1. Mố số ký tụe đặc biệt trong string:
+ *    \': biểu dữn ký tự nháy đơn.
+ *    \": biểu diễn ký tự nháy kép "
+ *    \\: biểu diễn ký tự back slash \
+ *    \r: đầu dòng
+ *    \t: tab
+ * 
+ *  2. kiểu dữ liệu Number:
+ *    trong js có thể xem là kiểu tổng hợp của int, float, double,... trong các ngôn ngữ lập trình khác
+ *    trong kiểu number này cần lưu ý hai giá trị đặc biệt:
+ *      infinity: là số "vô cực"
+ *      NaN: là not a Number
+ * 
+ *  3. kiểu dữ liệu undefined:
+ *    trong js, khi một biến đượckhai báo mà ko gắn vs bất kỳ giá trị nào thì biến đó gọi là underfine
+ * 
+ *  4. kiểu dữ liệu Null
+ *  typeOf(underfine) = underfine
+ *  typeOf(null) = Object
+ * 
+ * 
+ */
+
 function removeEnd(arr, n) {
     // your code here!
     var temp = new Array[arr.length];
